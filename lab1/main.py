72cade32885a9fcdd508e1b7581c3c5707379259
SantonioTheFirst <santonio123@mail.ru>
2021-09-29 17:25:44 +0300

    calc_PMC method added

--- lab1/main.py
+++ lab1/main.py
@@ -10,7 +10,7 @@ from scipy.stats import bernoulli as brnl
 import os
 
 
-# In[81]:
+# In[85]:
 
 
 class Lab1:
@@ -32,24 +32,63 @@ class Lab1:
         return PC
     
     
+    def calc_PMC(self):
+        PMC = np.zeros((self.table.shape[0], self.table.shape[0]), dtype=np.float64)
+        for i in range(self.table.shape[0]):
+            X, Y = np.where(self.table == i)
+            for x, y in zip(X, Y):
+                PMC[y][i] += self.prob.iloc[0, y] * self.prob.iloc[1, x]
+        return PMC
+    
+    
     def __repr__(self):
         return 'Lab1 is not so clear :('
 
 
-# In[82]:
+# In[86]:
 
 
 a = Lab1('vars', 10)
 
 
-# In[83]:
+# In[87]:
 
 
 res = a.calc_PC()
 
 
-# In[84]:
+# In[96]:
 
 
-assert sum(res) == 1.0, 'Should be 1.0'
+assert res.sum() == 1.0, 'Should be 1.0'
+
+
+# In[97]:
+
+
+res1 = a.calc_PMC()
+
+
+# In[98]:
+
+
+#get_ipython().run_cell_magic('timeit', '', 'sum(sum(res1))')
+
+
+# In[99]:
+
+
+#get_ipython().run_cell_magic('timeit', '', 'res1.sum()')
+
+
+# In[100]:
+
+
+assert res1.sum() == 1.0, 'Should be 1.0'
+
+
+# In[ ]:
+
+
+
 
