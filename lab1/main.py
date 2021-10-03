ed0dbd8b35296a3c3bf72801459fca272e6f5eab
SantonioTheFirst <santonio123@mail.ru>
2021-10-03 14:06:56 +0300

    calc_PM_C and calc_Bayes methods added

--- lab1/main.py
+++ lab1/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[80]:
+# In[1]:
 
 
 import numpy as np
@@ -10,7 +10,7 @@ from scipy.stats import bernoulli as brnl
 import os
 
 
-# In[85]:
+# In[402]:
 
 
 class Lab1:
@@ -24,71 +24,100 @@ class Lab1:
     
     
     def calc_PC(self):
-        PC = np.zeros(self.table.shape[0], dtype=np.float64)
+        PC = np.zeros(self.table.shape[0])
         for i in range(self.table.shape[0]):
             X, Y = np.where(self.table == i)
             for x, y in zip(X, Y):
                 PC[i] += self.prob.iloc[0, y] * self.prob.iloc[1, x]
-        return PC
+        return np.around(PC, 4)
     
     
     def calc_PMC(self):
-        PMC = np.zeros((self.table.shape[0], self.table.shape[0]), dtype=np.float64)
+        PMC = np.zeros((self.table.shape[0], self.table.shape[0]))
         for i in range(self.table.shape[0]):
             X, Y = np.where(self.table == i)
             for x, y in zip(X, Y):
                 PMC[y][i] += self.prob.iloc[0, y] * self.prob.iloc[1, x]
-        return PMC
+        return np.around(PMC, 4)
     
+    
+    def calc_PM_C(self, PC, PMC):
+        PC = PC.T
+        PM_C = np.zeros((PMC.shape[0], PMC.shape[0]))
+        for i in range(PMC.shape[0]):
+            PM_C[i] = PMC[i] / PC
+        return np.around(PM_C, 4)
+    
+    
+    def cal_Bayes(self, PC, PM_C):
+        temp = np.zeros(PC.shape[0], dtype=np.int64)
+        loss = 0
+#         print(PC.shape)
+#         PC_T = PC.T
+#         print(PC.shape)
+        PM_C_T = PM_C.T
+        for i in range(PM_C_T.shape[0]):
+            temp[i] = int(np.where(PM_C_T[i] == PM_C_T[i].max())[0][0])
+#             print(type(temp[i]))
+            print(f'M[{temp[i]}] ===> C[{i}]')
+        
+        for i in range(PC.shape[0]):
+#             print(temp[i])
+            loss += PC[i] * (1 - PM_C[int(temp[i])][i])
+        print(f'Loss: {loss}')
+        return temp
+        
     
     def __repr__(self):
         return 'Lab1 is not so clear :('
 
 
-# In[86]:
+# In[403]:
 
 
-a = Lab1('vars', 10)
+a = Lab1('vars', '01')
 
 
-# In[87]:
+# In[404]:
 
 
-res = a.calc_PC()
+PC = a.calc_PC()
+PC
 
 
-# In[96]:
+# In[405]:
 
 
-assert res.sum() == 1.0, 'Should be 1.0'
+assert res.sum().round(10) == 1.0, 'Should be 1.0'
 
 
-# In[97]:
+# In[406]:
 
 
-res1 = a.calc_PMC()
+PMC = a.calc_PMC()
 
 
-# In[98]:
+# In[407]:
 
 
-#get_ipython().run_cell_magic('timeit', '', 'sum(sum(res1))')
+assert PMC.sum().round(10) == 1.0, 'Should be 1.0'
 
 
-# In[99]:
+# In[408]:
 
 
-#get_ipython().run_cell_magic('timeit', '', 'res1.sum()')
+PM_C = a.calc_PM_C(PC, PMC)
+PM_C
 
 
-# In[100]:
+# In[409]:
 
 
-assert res1.sum() == 1.0, 'Should be 1.0'
+assert PM_C.sum().round(3) == 20.0, 'Should be 20.0'
 
 
-# In[ ]:
+# In[410]:
 
 
-
+a.cal_Bayes(PC, PM_C)
 
